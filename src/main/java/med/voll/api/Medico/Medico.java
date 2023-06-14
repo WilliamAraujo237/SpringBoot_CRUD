b5d9c422c177c3d8a43b7261f3f0b374bc3b9513
William Araújo <william237.araujo@gmail.com>
2023-06-13 21:49:08 -0300

    teste

---
+++ src/main/java/med/voll/api/Medico/Medico.java
@@ -0,0 +1,12 @@
+package med.voll.api.Medico;
+
+import med.voll.api.Endereco.Endereco;
+
+public class Medico {
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+    private Especialidade especialidade;
+    private Endereco endereco;
+}
