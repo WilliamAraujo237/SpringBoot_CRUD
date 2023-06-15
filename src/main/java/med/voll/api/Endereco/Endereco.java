185e885596d42e37e4492b2e29c3a1329af56f7c
William Araújo <william237.araujo@gmail.com>
2023-06-15 10:32:30 -0300

    adicionando marcação Embeddable na class Endereço

--- src/main/java/med/voll/api/Endereco/Endereco.java
+++ src/main/java/med/voll/api/Endereco/Endereco.java
@@ -1,4 +1,7 @@
 package med.voll.api.Endereco;
 
+import jakarta.persistence.Embeddable;
+
+@Embeddable
 public record Endereco(String logradouro,String bairro,String cep,String cidade,String uf,String numero, String complemento) {
 }
