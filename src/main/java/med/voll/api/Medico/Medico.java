2f94e956df8b9ed8df471525964f92282c7ba58d
William Araújo <william237.araujo@gmail.com>
2023-06-15 10:29:08 -0300

    Adicionando marcações JPA na classe Médico

--- src/main/java/med/voll/api/Medico/Medico.java
+++ src/main/java/med/voll/api/Medico/Medico.java
@@ -1,12 +1,17 @@
 package med.voll.api.Medico;
 
+import jakarta.persistence.*;
 import med.voll.api.Endereco.Endereco;
-
+@Table(name = "Medico")
+@Entity(name = "Medico")
 public class Medico {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
     private String crm;
+    @Enumerated
     private Especialidade especialidade;
+    @Embedded
     private Endereco endereco;
 }
